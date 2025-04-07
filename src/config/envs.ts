6cd367dbb5c6458ceb5b68dde5ba65ef0ee24ab7
Frysccou <franespindola71@gmail.com>
2025-04-07 12:20:57 -0300

    deploy v2

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 3000;
-export const DB_NAME: string = process.env.DB_NAME || "proyecto_m4_front";
-export const DB_USER: string = process.env.DB_USER || "postgres";
-export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
-export const DB_HOST: string = process.env.DB_HOST || "localhost";
-export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
 export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
+
+export const DATABASE_URL: string = process.env.DATABASE_URL || "";
+
+if (!DATABASE_URL) {
+	throw new Error("DATABASE_URL is not defined in environment variables");
+}
